233de326447af981ebf5cafabf63850b27f00247
ekimaycicek <ekimaycicek@icloud.com>
2023-05-29 12:57:08 +0300

    new

---
+++ src/main/java/NestedLoopSoru3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NestedLoopSoru3 {
+
+    public static void main(String[] args) {
+        /*
+        girilen bir ifadenin istenen harf sayısını print eden code create ediniz
+
+        input : selam javaCAN'lar
+        output : a sayısı :3
+         */
+        Scanner sc=new  Scanner(System.in);
+
+
+        System.out.println("bir cumle giriniz : ");
+
+        String  cumle=sc.nextLine();
+        System.out.println("sayılacak harfi giriniz : ");
+
+        char harf=sc.next().charAt(0);
+        int harfSayisi=0;//ilk değer 0 atandıkı sayma da etki etmesin
+
+
+        for (int i = 0; i <cumle.length(); i++) {
+
+            if(cumle.charAt(i)==harf){
+                harfSayisi++;
+
+            }
+
+        }
+
+        System.out.println("girdiğiniz cümle: " + cumle+"de istediğiniz harf : " + harf + harfSayisi+ "kadar var" );
+
+
+
+    }
+}
